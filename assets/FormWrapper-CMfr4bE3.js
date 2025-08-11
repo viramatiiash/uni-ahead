f3b95b7cd696412f4f44510e4874759908b547c0
Vira <virko1996@gmail.com>
2025-08-11 22:03:57 +0300

    Updates

---
+++ assets/FormWrapper-CMfr4bE3.js
@@ -0,0 +1,1 @@
+import{j as p,c as a}from"./index-DSKJ8E2h.js";const e="formWrapper__McKNk",o={formWrapper:e},t=({className:r,children:s})=>p.jsx("div",{className:a(o.formWrapper,r),children:s});export{t as F};
